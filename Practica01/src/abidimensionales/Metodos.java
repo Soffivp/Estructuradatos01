71a4a6b0f3fc5bb3849d907e0a24234644e33016
Richard Ortiz <raortiz20@utpl.edu.ec>
2023-05-16 16:45:22 -0500

    arreglos bidimensionales(arreglar)

--- Practica01/src/abidimensionales/Metodos.java
+++ Practica01/src/abidimensionales/Metodos.java
@@ -5,10 +5,77 @@
  */
 package abidimensionales;
 
+import java.util.Scanner;
+
 /**
  *
  * @author SALA H
  */
 public class Metodos {
-      
+
+    // transponer, multiplicar, 
+    Scanner entrada;
+
+    public Metodos() {
+        this.entrada = new Scanner(System.in);
+
+    }
+    // 1,2,3,4 5,6,7,8 9,10,11,12 13,14,15,16
+
+    public int menu() {
+        System.out.println("1. Transponer la matriz en la diagonal principal");
+        System.out.println("2. Transponer la matriz en la diagonal secundaria");
+        System.out.println("3. Mulpiplicar matriz a * b");
+        System.out.println("4. MCD");
+        System.out.println("Salir");
+        return entrada.nextInt();
+    }
+
+    public int[][] transponerprincipal(int[][] a) {
+        int[][] b = new int[4][4];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                b[i][j] = a[j][i];
+
+            }
+        }
+        return b;
+    }
+
+    public int[][] transponersecundaria(int[][] a) {
+        int[][] b = new int[4][4];
+        int k = 0;
+        int l = 0;
+        int aux;
+        for (int i = 0; i < a.length ; i++) {
+            k = a[i].length-1;
+            for (int j = 0; j < a[i].length; j++) {
+                aux = a[i][j];
+                a[i][j] = a[k][l];
+                a[k][l] = aux;
+                k--;
+            }
+            l--;
+        }
+        return a;
+    }
+        public int[][] multiplicacion(int[][] a, int[][] b) {
+        int[][] resultado = new int[4][4];
+        int k = 0;
+        int suma = 0;
+        for (int i = 0; i < a.length - 1 ; i++) {
+            k = a[i].length-1;
+            
+            suma = 0;
+            for (int l= 0; l < b[i].length; l++) {
+                for (int j = 0; j < b.length; j++) {
+                    suma = a[i][j]*b[j][l];
+                    
+                }
+                resultado[i][l] = suma;
+            }
+       
+        }
+        return a;
+    }
 }
--- Practica01/src/ejercicios/Metodos.java
+++ Practica01/src/ejercicios/Metodos.java
@@ -5,7 +5,62 @@ package ejercicios;
  * @author SALA H
  */
 public class Metodos {
+
+//ejercicio 1 Calcular el número de elementos negativos, cero y positivos de un vector dado de n
+//elementos    
+    public void contarnumeros(int[] a) {
+        int positivos = 0, negativos = 0, cero = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 0) {
+                cero++;
+            } else if (a[i] % 2 == 0) {
+                positivos++;
+            } else if (a[i] % 2 != 0) {
+                negativos++;
+            }
+        }
+        System.out.printf("Existen %d números positivos, %d numeros negativos y %d ceros.", positivos, negativos, cero);
+
+    }
+
+    //ejercicio2   Escriba un programa que me permita almacenar en un arreglo B, el factorial de 10
+//números almacenados previamente en un arreglo A.
+    // este arreglo se va a llevar en el main al enviarle cada uno de los numeros
+    public int factorial(int[] a) {
+        int b = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            b = a[i];
+
+            if (b == 1) {
+                return b;
+            } else {
+                return b * factorial(b - 1);
+            }
+        }
+
+        return b;
+    }
+// ejercicio 3 
+
+    public int fibonaci(int num) {
+      //  int num = 0;
+
+            if (num == 0) {
+                return 0;
+            } else if (num == 1) {
+                return 1;
+            } else {
+                return fibonaci(num - 1) + fibonaci(num - 2);
+            }
+            
+        
+       // num++;
+    return num;
+    }
 // ejercicio 6
+
     public void llenarArreglo(int[] a, int num) {
         double b;
         for (int i = 0; i < num; i++) {
@@ -18,14 +73,14 @@ public class Metodos {
         int[] b = null;
         for (int i = 0; i < a.length; i++) {
             b[i] = a[i] / num;
-                                                                      
+
         }
 
     }
 
     public void presentar(int[] b) {
         for (int i = 0; i < b.length; i++) {
-            System.out.println("- " + b[i]);   
+            System.out.println("- " + b[i]);
         }
     }
 // ejercicios 7    
